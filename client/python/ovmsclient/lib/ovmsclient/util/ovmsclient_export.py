0d471feb8d0abf136ef75a9b1e92ba8b1c10ca49
Brett Thompson <196701379+brettmthompson@users.noreply.github.com>
2026-02-25 16:52:23 -0500

    another change
    Signed-off-by: Brett Thompson <196701379+brettmthompson@users.noreply.github.com>

--- client/python/ovmsclient/lib/ovmsclient/util/ovmsclient_export.py
+++ client/python/ovmsclient/lib/ovmsclient/util/ovmsclient_export.py
@@ -26,6 +26,7 @@ class api_export(object):
 
     def __init__(self, name, **kwargs):
         # Test Change
+        # Another change
         self.main_name = name
         self.namespaced_names = kwargs
 
