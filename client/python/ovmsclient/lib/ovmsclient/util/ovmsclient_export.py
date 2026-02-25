a25bae18d38f897d1a86a5b5ff9138fb8be7db2a
Brett Thompson <196701379+brettmthompson@users.noreply.github.com>
2026-02-25 16:28:18 -0500

    test-change
    Signed-off-by: Brett Thompson <196701379+brettmthompson@users.noreply.github.com>

--- client/python/ovmsclient/lib/ovmsclient/util/ovmsclient_export.py
+++ client/python/ovmsclient/lib/ovmsclient/util/ovmsclient_export.py
@@ -25,8 +25,6 @@ class api_export(object):
     """Provides ways to export symbols to the ovmsclient API."""
 
     def __init__(self, name, **kwargs):
-        # This is a test
-        # This is another test
         self.main_name = name
         self.namespaced_names = kwargs
 
